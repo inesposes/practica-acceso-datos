a3cd8143e944a2cd39237872c073f287832e6a9d
inesposes <inesindev@gmail.com>
2024-12-11 18:52:32 +0100

    correccions insercion

--- scripts/mongo_insertion.py
+++ scripts/mongo_insertion.py
@@ -3,27 +3,21 @@ import requests
 # from mongo_connection import connect_mongo
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://mongo_db:27017/')
 db = client['bicicorunha']
 collection =db['stations']
 # client, collection=connect_mongo()
 print(collection)
-print('Insertingg...')
+print('Inserting...')
 while True: 
-    print('entro')
     #API Request
     endpoint="http://api.citybik.es/v2/networks/bicicorunha"
     response=requests.get(endpoint)
     response_json=response.json()
     stations=response_json['network']['stations']
-    print('sigo')
-
     try:
         # Insert in MongoDB
-        print("try")
         result = collection.insert_many(stations)
-        print('Inserted')
     except Exception as e:
-        print("error")
-    print("salgo")
-    time.sleep(2) #Executed every 300sec (5min)
+        print("error"+ str(e))
+    time.sleep(300) #Executed every 300sec (5min)
